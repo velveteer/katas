450eb5960db308bb6ee7abb4d335124490481d8b
Josh Miller <notjoshmiller@gmail.com>
2018-08-01 18:39:26 +0000

    add drone fly by kata

---
+++ test/DroneFlyBySpec.hs
@@ -0,0 +1,13 @@
+module DroneFlyBySpec (spec) where
+
+import Test.Hspec
+import Katas.DroneFlyBy (flyBy)
+
+spec :: Spec
+spec = do
+  describe "Drone Fly-By" $ do
+    it "Example Tests" $ do
+      flyBy "xxxxxx" "====T" `shouldBe` "ooooox"
+      flyBy "xxxxxxxxx" "==T" `shouldBe` "oooxxxxxx"
+      flyBy "xxxxxxxxxxxxxxx" "=========T" `shouldBe` "ooooooooooxxxxx"
+      flyBy "x" "=T" `shouldBe` "o"
