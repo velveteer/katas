822a214e652a1746fca6bd23f99faef1a658c69e
Josh Miller <notjoshmiller@gmail.com>
2018-08-01 16:23:25 +0000

    add sum odd numbers kata template

--- src/Katas/LostWithout.hs
+++ src/Katas/LostWithout.hs
@@ -1,4 +1,12 @@
 module Katas.LostWithout where
 
+-- Given an array of integers, return a new array with each value doubled.
+
+-- For example:
+
+-- [1, 2, 3] --> [2, 4, 6]
+
+-- For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+
 maps :: [Int] -> [Int]
 maps xs = map (\x -> x * 2) xs
---
+++ test/SumOddsSpec.hs
@@ -0,0 +1,20 @@
+module SumOddsSpec (spec) where
+
+import Katas.SumOdds (rowSumOddNumbers)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "rowSumOddNumbers" $ do
+    it "should return 8 on input 2" $
+      rowSumOddNumbers 2 `shouldBe` 8
+
+    it "should return 2197 on input 13" $
+      rowSumOddNumbers 13 `shouldBe` 2197
+
+    it "should return 6859 on input 19" $
+      rowSumOddNumbers 19 `shouldBe` 6859
+
+    it "should return 970299 on input 99" $
+      rowSumOddNumbers 99 `shouldBe` 970299
+
